007a2c4d32dcbb9f22a933992a00b9c71c64f3c0
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-12-01 21:11:09 -0500

    CopyPlugin added to the config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
@@ -37,6 +38,22 @@ module.exports = {
 		}),
 		new MiniCssExtractPlugin({
 			filename: './styles/[name].[contenthash].css',
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src', 'assets/data'),
+					to: './assets/data',
+				},
+				{
+					from: path.resolve(__dirname, 'src', 'assets/favicon'),
+					to: './assets/favicon',
+				},
+				{
+					from: path.resolve(__dirname, 'src', 'assets/images'),
+					to: './assets/images',
+				},
+			],
 		}),
 	],
 };
