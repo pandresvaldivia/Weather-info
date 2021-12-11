f9dfb2571fb218e3144acca1363a3cb1c84a5825
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-12-10 22:11:04 -0500

    Week weather list formatter function created

--- src/js/weekly-weather.js
+++ src/js/weekly-weather.js
@@ -7,7 +7,25 @@ async function weeklyWeather() {
 	if (error) return console.error(error);
 
 	const weatherData = await getWeatherInfo(latitude, longitude, 'forecast');
+
 	if (weatherData.error) return new Error(weatherData.error);
+	console.log(formatWeekList(weatherData.list));
+}
+
+function formatWeekList(rawData) {
+	const weekList = [];
+	let dayList = [];
+
+	rawData.forEach((dayHour, index) => {
+		dayList.push(dayHour);
+
+		if ((index + 1) % 8 === 0) {
+			weekList.push(dayList);
+			dayList = [];
+		}
+	});
+
+	return weekList;
 }
 
 export { weeklyWeather };
